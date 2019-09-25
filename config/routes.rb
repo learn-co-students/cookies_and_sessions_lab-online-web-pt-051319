a29b09fb6d5ccab194e623483190fae41fe8fa4b
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-09-25 19:08:16 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/', to: 'application#homepage'
+  post '/add', to: 'application#add'
+  root 'application#homepage'
 end
